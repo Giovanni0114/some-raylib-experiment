b3cb28b23434dc0b44d74e34f1781630592f02fd
Jan Juroszek <janko.juroszek@gmail.com>
2024-01-26 00:32:59 +0100

    implement working solution for creating menu

--- menu.c
+++ menu.c
@@ -1,76 +1,101 @@
+#include <assert.h>
 #include <raylib.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef void (*FunctionPointer)();
-typedef struct MenuObject{
+typedef void (*FunctionPointer)(void);
+typedef struct {
     char name[15];
     char displayName[30];
     FunctionPointer function;
-} Menu;
+} MenuObject;
 
 // MENU ELEMENT START
 #define _X(x) (x + 370)
 #define _Y(y) (y + 20)
+
+FunctionPointer activeMenuObjectFunction = NULL;
 
 void drawAvatarMenu(){
     // color picker
 };
 
 void drawVolumeMenu(){
-    
+
 };
 void drawGameSettingsMenu(){};
-void drawCreditsMenu(){
-    DrawText("WWWWWW", _X(20), _Y(20), 24, BLACK);
+void drawCreditsMenu() { 
+    DrawText("Author:", _X(20), _Y(20), 24, BLACK); 
+    DrawText("giovanni", _X(30), _Y(50), 24, BLACK); 
+    DrawText("<janko.juroszek@gmail.com>", _X(30), _Y(75), 24, BLACK); 
 };
+void exitGame() {
+    exit(0);
+}
+
 
 char activeObject[15] = {};
 
-const struct MenuObject menuObjects[] = {
+const MenuObject s_menuObjects[] = {
     {"avatar", "Avatar appearance", drawAvatarMenu},
-    {"volume","Volume", drawVolumeMenu},
-    {"game-settings","Game Settings", drawGameSettingsMenu},
+    {"volume", "Volume", drawVolumeMenu},
+    {"game-settings", "Game Settings", drawGameSettingsMenu},
     {"credits", "Credits", drawCreditsMenu},
+    {"exit", "Exit", exitGame},
 };
 
-const struct MenuObject* lookupObjects(const char name[15]){
-    for (int i = 0; i < sizeof(menuObjects)/sizeof(struct MenuObject); i++) {
-        if (menuObjects[i].name == name) {
-            return (menuObjects + i);
+const MenuObject* lookupObjects(const char name[15]) {
+    for (uint i = 0; i < sizeof(s_menuObjects) / sizeof(MenuObject); i++) {
+        if (s_menuObjects[i].name == name) {
+            return &(s_menuObjects[i]);
         }
     }
     return NULL;
 };
 
-bool isCursorAboveMenuObject(int idx){
-    int x = GetMouseX(); int y = GetMouseY();
+bool isCursorAboveMenuObject(int idx) {
+    int x = GetMouseX();
+    int y = GetMouseY();
     bool ret = true;
     ret &= x > 20;
     ret &= x < 350;
-    ret &= y > 40 + (idx*50);
-    ret &= y < 40 + (idx*50) + 40;
+    ret &= y > 40 + (idx * 50);
+    ret &= y < 40 + (idx * 50) + 40;
     if (ret) SetMouseCursor(MOUSE_CURSOR_CROSSHAIR);
     return ret;
 }
 
-void showMenu(){
-    bool isCursorAboveNoneBlocks = false;
+void showMenu() {
+    bool isCursorAboveNoneBlocks = true;
 
-    for (int i = 0; i < sizeof(menuObjects)/sizeof(struct MenuObject); i++) {
-        if (isCursorAboveMenuObject(i)){
-            isCursorAboveNoneBlocks = true;
+    for (uint i = 0; i < sizeof(s_menuObjects) / sizeof(MenuObject); i++) {
+        DrawRectangle(20, 40 + (i * 50), 330, 40, isCursorAboveMenuObject(i) ? SKYBLUE : BLUE);
+        DrawText(s_menuObjects[i].displayName, 30, 45 + (i * 50), 30, RAYWHITE);
+
+        if (isCursorAboveMenuObject(i)) {
+            isCursorAboveNoneBlocks = false;
+
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                if (activeMenuObjectFunction == NULL || activeMenuObjectFunction != s_menuObjects[i].function) {
+                    activeMenuObjectFunction = s_menuObjects[i].function;
+                } else {
+                    activeMenuObjectFunction = NULL;
+                }
+            }
         }
-        DrawRectangle(20, 40 + (i*50), 330, 40, isCursorAboveMenuObject(i)?SKYBLUE:BLUE);
-        DrawText(menuObjects[i].displayName, 30, 45 + (i*50), 30, RAYWHITE);
     }
 
     DrawLine(370, 20, 370, 430, RED);
     DrawLine(770, 20, 770, 430, RED);
 
-
-    if (!isCursorAboveNoneBlocks){
+    if (isCursorAboveNoneBlocks) {
         SetMouseCursor(MOUSE_CURSOR_DEFAULT);
     }
+
+    if (activeMenuObjectFunction == NULL) {
+        return;
+    }
+
+    activeMenuObjectFunction();
 }
 
